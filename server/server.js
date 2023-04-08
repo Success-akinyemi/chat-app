370b93fbbfbde5b3191096fcbc45fe711e7b124c
Success-akinyemi <brownsamuel915@gmail.com>
2023-04-07 20:20:24 -0700

    Cors change 2

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'https://chimerical-travesseiro-c18bde.netlify.app/',
+        origin: 'https://chimerical-travesseiro-c18bde.netlify.app',
         //'http://localhost:3000',
         methods: ['GET', 'POST']
     }
