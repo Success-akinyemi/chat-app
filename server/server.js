0d29367e0cdb4abdf6239d1381867644365863d3
Success-akinyemi <brownsamuel915@gmail.com>
2023-04-07 19:56:05 -0700

    Cors change 2

--- server/server.js
+++ server/server.js
@@ -9,11 +9,7 @@ const Message = require('./models/Message')
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use((req, res,next) => {
-    res.setHeader('Access-Control-Allow-Origin','https://chimerical-travesseiro-c18bde.netlify.app/'),
-    res.setHeader('Access-Control-Allow-Headers', '*'),
-    next();
-})
+
 app.use(cors());
 
 app.use('/users', userRoutes)
